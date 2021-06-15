3f3c613d59dcc078843ed898fdec881e26d1a9e5
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-06-15 18:05:51 -0500

    Added alerts and confirmations

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,39 @@ var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 var numberic = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '~', '`', '[', ']', '{', '}', ';', ':', '"', ',', '<', '>', '.', '/', '?', '|'];
 
+
+function getPassword() {
+    var length = parseInt(
+        window.prompt('How many characters would you like your password to have?')
+
+    );
+}
+
+if (length < 8) {
+    alert('The password must be at least 8 characters');
+}
+
+if (length > 128) {
+    alert('The password must be less than 129 characters');
+}
+
+var confirmLowerCase = confirm('Click OK to confirm including lowercase characters.');
+var confirmUpperCase = confirm('Click OK to confirm including uppercase characters.');
+var confirmNumber = confirm('Click OK to confirm including numeric characters.');
+var confirmSpecialCharacter = confirm('Click OK to confirm including special characters.');
+
+if (
+    confirmLowerCase === false &&
+    confirmUpperCase === false &&
+    confirmNumber === false &&
+    confirmSpecialCharacter === false 
+) 
+{
+    alert('Please select at least one option');
+}
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
