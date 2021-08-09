613247e596819e716cfdde6c8048fce5190ec4b1
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-08-09 16:46:53 -0500

    created functions to generate

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,33 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("generate");
+var passwordText = document.querySelector("password");
+
+function generateLower() {
+    var lower = 'abcdefghijklmnopqrstuvwxyz'
+    return lower[Math.floor(Math.random() * 26)]
+}
+
+function generateUpper() {
+    var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return upper[Math.floor(Math.random() * 26)]
+}
+
+function generateCharacter() {
+    var character = '~!@#$%^&*()_+=?><'
+    return character[Math.floor(Math.random() * character.length)]
+}
+
+function generateNumber() {
+    return Math.floor(Math.random() * 10)
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
